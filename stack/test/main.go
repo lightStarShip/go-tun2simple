27cdb246cbfb33fb6e8d2518db778c584b40afcf
hopwesley <ws@sinoc.io>
2022-07-01 18:04:31 +0800

    add a test

--- stack/test/main.go
+++ stack/test/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"github.com/lightStarShip/go-tun2simple/core"
+	"io"
+	"net"
 )
 
 var (
@@ -26,13 +29,98 @@ func decode(s string) []byte {
 }
 
 func main() {
-	s := core.Inst()
-	sig := make(chan struct{}, 1)
-	for _, d := range tcpRaw {
-		tcpPayLoad := decode(d)
-		n, e := s.InputIpPackets(tcpPayLoad)
-		fmt.Println(n, e)
+	src, err := net.ListenTCP("tcp", &net.TCPAddr{
+		IP:   nil,
+		Port: 18888,
+	})
+	if err != nil {
+		panic(err)
 	}
 
-	<-sig
+	for {
+		conn, err := src.AcceptTCP()
+		if err != nil {
+			panic(err)
+		}
+		go relay(conn)
+	}
+}
+
+type TestProxySync struct {
+	Target string
+}
+
+type TestProxyAck struct {
+	msg string
+}
+type duplexConn interface {
+	net.Conn
+	CloseWrite() error
+	CloseRead() error
+}
+
+func handleInput(conn net.Conn, input io.ReadCloser) {
+	defer func() {
+		if tcpConn, ok := conn.(core.TCPConn); ok {
+			tcpConn.CloseWrite()
+		} else {
+			conn.Close()
+		}
+		if tcpInput, ok := input.(duplexConn); ok {
+			tcpInput.CloseRead()
+		} else {
+			input.Close()
+		}
+	}()
+
+	io.Copy(conn, input)
+}
+
+func handleOutput(conn net.Conn, output io.WriteCloser) {
+	defer func() {
+		if tcpConn, ok := conn.(core.TCPConn); ok {
+			tcpConn.CloseRead()
+		} else {
+			conn.Close()
+		}
+		if tcpOutput, ok := output.(duplexConn); ok {
+			tcpOutput.CloseWrite()
+		} else {
+			output.Close()
+		}
+	}()
+
+	io.Copy(output, conn)
+}
+
+func relay(conn *net.TCPConn) {
+	buf := make([]byte, 1<<20)
+	n, err := conn.Read(buf)
+	if err != nil {
+		panic(err)
+	}
+
+	sync := &TestProxySync{}
+	if err := json.Unmarshal(buf[:n], sync); err != nil {
+		panic(err)
+	}
+	fmt.Println("new conn------>", sync.Target)
+
+	targetConn, err := net.Dial("tcp", sync.Target)
+	if err != nil {
+		data, _ := json.Marshal(&TestProxyAck{
+			msg: err.Error(),
+		})
+		conn.Write(data)
+		fmt.Println(err)
+		return
+	}
+
+	data, _ := json.Marshal(&TestProxyAck{
+		msg: "OK",
+	})
+	conn.Write(data)
+
+	go handleInput(conn, targetConn)
+	handleOutput(conn, targetConn)
 }
